471d0c96f0a5a711ce6835e4051be6732ba5ae07
kkristof200 <kovacskristof200@gmail.com>
2020-12-06 14:38:03 +0200

    no message

--- solutions/06.py
+++ solutions/06.py
@@ -1,3 +1,10 @@
+from typing import List
+
+def f1(groups: List[List[str]]) -> None:
+    print('f1:', sum([len(l) for l in groups]))
+
 from input_loader import load_input
 
-print(len(load_input()))
+groups = [list({str(c) for c in group.replace('\n', '')}) for group in load_input().split('\n\n')]
+
+f1(groups)
